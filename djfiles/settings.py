857839b90ab2e3f7a34dee67ae80cd56ee6d45c5
FTB-Developer1 <94354519+FTB-Developer1@users.noreply.github.com>
2022-03-20 13:35:06 +1100

    static

--- djfiles/settings.py
+++ djfiles/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'web'
+    'web',
 ]
 
 MIDDLEWARE = [
@@ -72,7 +72,6 @@ TEMPLATES = [
         },
     },
 ]
-
 WSGI_APPLICATION = 'djfiles.wsgi.application'
 
 
@@ -117,11 +116,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 
--- djfiles/urls.py
+++ djfiles/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from web import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about, name="about"),
-    path('kynan/', views.kynan, name="kynan")
-]
+    path('kynan/', views.kynan, name="kynan"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
